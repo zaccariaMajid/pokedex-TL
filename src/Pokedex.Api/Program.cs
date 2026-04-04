ca5d5389d809d0e7b184710ecb00e2c8f57018f1
zack <zaccariam45@gmail.com>
2026-04-04 23:38:23 +0200

    feat: add Swagger integration for API documentation

--- src/Pokedex.Api/Program.cs
+++ src/Pokedex.Api/Program.cs
@@ -4,6 +4,8 @@ using Pokedex.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddProblemDetails();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
@@ -11,6 +13,8 @@ builder.Services.AddInfrastructure();
 var app = builder.Build();
 
 app.UseExceptionHandler();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
 app.MapControllers();
 
